2dc814eecb25600bfb21a49da53ca910b6c0d34f
fdo <filimonenkov@stips.io>
2019-10-07 17:34:05 +0500

    имя администратора сменено на admin

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 $login = '';
 $msg = '';
 if (isset($_POST['login']))  {
-	if (($_POST['login'] == 'administrator') && isset($_POST['pwd']) && ($_POST['pwd'] == '123')) {
+	if (($_POST['login'] == 'admin') && isset($_POST['pwd']) && ($_POST['pwd'] == '123')) {
 		session_start();
 		setcookie('cv', md5('AdministrAtor'));
 		header('Location: /index.php');
